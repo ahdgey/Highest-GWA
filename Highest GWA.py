77c4f3c2cfe794b6c0bbecac149df2feaf1af7db
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-26 23:43:21 +0800

    Programmers name

--- Highest GWA.py
+++ Highest GWA.py
@@ -6,3 +6,5 @@
 print("\033[0;36m*" * 90)
 print("\033[1;97mA program that read a file conatining the name of 20 students together with their GWA".center(97))
 print("\033[0;36m*" * 90)
+
+print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
