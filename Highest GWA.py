0ef3be10c8fa4fd30f06e0131604ca252a7ed140
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-26 23:38:13 +0800

    Title

--- Highest GWA.py
+++ Highest GWA.py
@@ -1,3 +1,8 @@
 #Alexza Jean R. Catanoy
 #BSCPE 1-4
 #Highest GWA
+
+#Title
+print("\033[0;36m*" * 90)
+print("\033[1;97mA program that read a file conatining the name of 20 students together with their GWA".center(97))
+print("\033[0;36m*" * 90)
